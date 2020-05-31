4ddf9e157d7958a127e37593cbae5186b04c8fe8
agga787 <agga787@usc.edu>
2020-05-31 08:30:13 +0000

    Next Commit

--- OsMaikada/settings.py
+++ OsMaikada/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm&z$l(a1=&akixy--=ccdgbo*@4@gf&mtwp1i8)-jf9o91i+ls'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.oshomaikada.com']
 
 
 # Application definition
