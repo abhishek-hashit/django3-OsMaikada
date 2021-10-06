247272d71d8372fd6addb8203acee2469a4c4c74
Abhishek Aggarwal <abhishek.aggarwal@gurucul.com>
2021-10-06 14:46:51 +0530

    First Commit

--- OsMaikada/settings.py
+++ OsMaikada/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+try:
+    from .local_settings import *
+except ImportError:
+    print("On Live istance..")
